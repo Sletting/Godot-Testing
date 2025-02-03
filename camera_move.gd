4ce20f0f5b45b5f3940001640bbcca5d4555291e
Sletting <jonasplaying@outlook.com>
2025-02-03 22:10:12 +0100

    better movement with deadzones

--- camera_move.gd
+++ camera_move.gd
@@ -1,37 +1,38 @@
 extends Camera2D
 
-@export var camera_attach: Marker2D;
-@export var movement_range_px = 50
-@export var movement_threshold = 100
+@export var movement_range = 200
+@export var movement_threshold_percentage = 0.7
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# Get mouse position and
-	# Divide by viewport x and y to get a value ranging from -1 to 1
-	#		-1
-	# 	-1	 	 1
-	#		 1
-	var mousePosition: Vector2 = Vector2(get_local_mouse_position().x / (get_viewport().size.x / 2), get_local_mouse_position().y / (get_viewport().size.y / 2))
 	# Multiply with the maximum movement of the camera
-	var movementRange = mousePosition * movement_range_px
-	
+	var movementRange = get_normalized_deadzone_mouse_vector() * movement_range
 	# Set position to the difference
 	position = movementRange
-	
-	#var left = limit_left + (offset.x*zoom.x) + get_viewport().size.x * zoom.x / 10
-	#var right = limit_right - (offset.x*zoom.x) - get_viewport().size.x * zoom.x / 10
-	#var top = limit_top + (offset.y*zoom.y) + get_viewport().size.y * zoom.y / 10 
-	#var bottom = limit_bottom - (offset.y*zoom.y) - get_viewport().size.y * zoom.y / 10
-#
-	#position.x = clamp(mousePos.x, left, right)
-	#position.y = clamp(mousePos.y, top, bottom)
-	
-	
-	
-	#print(mousePos)
 	pass
+
+# Get mouse position and Divide by viewport x and y to get a value ranging from -1 to including a configurable deadzone before activation
+#		-1
+# 	-1	 	 1
+#		 1
+func get_normalized_deadzone_mouse_vector() -> Vector2:
+	var mousePos: Vector2 = get_local_mouse_position()
+	# Calculate x mouse position
+	var width: float = get_viewport().size.x * 0.5
+	if(mousePos.x > 0):
+		mousePos.x = clamp(mousePos.x - width * movement_threshold_percentage, 0, width) / width
+	else:
+		mousePos.x = clamp(mousePos.x + width * movement_threshold_percentage, width * -1, 0) / width
+	
+	# Calculate y mouse position
+	var height: float = get_viewport().size.y * 0.5
+	if(mousePos.y > 0):
+		mousePos.y = clamp(mousePos.y - height * movement_threshold_percentage, 0, height) / height
+	else:
+		mousePos.y = clamp(mousePos.y + height * movement_threshold_percentage, height * -1, 0) / height
+	
+	return mousePos
